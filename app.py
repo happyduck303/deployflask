120c46593f52a798c0a41ed88ff46c3cc56f54b9
M. Taufik Nelas <fickysyahreza.fs@gmail.com>
2020-05-28 16:21:22 +0700

    update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "HEHE .. 🦆 Quack .. Quack .."
+    return "there is no easter eggs here go away_ https://pbs.twimg.com/media/ECx9rziU8AAzDB4?format=jpg&name=small"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
